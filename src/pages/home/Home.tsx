f0df7cb16bbd849807a87b03885b91e16d5c5c57
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-07-31 21:29:12 +0300

    carousel

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,8 @@ import SearchButton from "../../components/searchButton";
 export default function Home() {
   return (
     <div className="homePage">
-      <Carousel />
+      {/* <Carousel /> */}
+      <img className="homeImage" src="https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60" alt="img" />
       <div>
         <SearchButton />
       </div>
